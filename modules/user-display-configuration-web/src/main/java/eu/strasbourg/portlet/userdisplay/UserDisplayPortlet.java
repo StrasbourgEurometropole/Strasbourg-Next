0978377b44efcc2166ca2d1ba4e997b178cc9714
medamineSully <ma.bourbah@sully-group.fr>
2023-03-24 13:59:00 +0100

    Migration Portlet front : user-display-configuration-web

--- modules/user-display-configuration-web/src/main/java/eu/strasbourg/portlet/userdisplay/UserDisplayPortlet.java
+++ modules/user-display-configuration-web/src/main/java/eu/strasbourg/portlet/userdisplay/UserDisplayPortlet.java
@@ -1,42 +1,26 @@
 package eu.strasbourg.portlet.userdisplay;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import eu.strasbourg.portlet.userdisplay.configuration.UserDisplayConfiguration;
+import eu.strasbourg.portlet.userdisplay.configuration.UserDisplayConfigurationDisplayContext;
+import eu.strasbourg.utils.PortletHelper;
+import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
+import org.osgi.service.component.annotations.Component;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
-import javax.portlet.PortletRequest;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 import javax.portlet.ResourceRequest;
 import javax.portlet.ResourceResponse;
 import javax.servlet.http.HttpServletRequest;
-
-import eu.strasbourg.portlet.userdisplay.configuration.UserDisplayConfiguration;
-import eu.strasbourg.portlet.userdisplay.configuration.UserDisplayConfigurationDisplayContext;
-import eu.strasbourg.utils.PortletHelper;
-import org.osgi.service.component.annotations.Component;
-
-import com.liferay.portal.convert.ConvertProcessUtil;
-import com.liferay.portal.kernel.json.JSONArray;
-import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
-import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.SessionParamUtil;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.kernel.util.WebKeys;
-
-import eu.strasbourg.service.oidc.model.PublikUser;
-import eu.strasbourg.service.oidc.service.PublikUserLocalServiceUtil;
-import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
+import java.io.IOException;
 
 /**
  * @author romain.vergnais
