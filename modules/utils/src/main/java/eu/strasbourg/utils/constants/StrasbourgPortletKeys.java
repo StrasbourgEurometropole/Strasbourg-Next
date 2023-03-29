444a23ff84cae74d33d69484fd4ffcf47c26f5a0
messagR <messagr@hotmail.com>
2023-03-29 11:21:07 +0200

    suppression ejob : suppression offer-bo

--- modules/utils/src/main/java/eu/strasbourg/utils/constants/StrasbourgPortletKeys.java
+++ modules/utils/src/main/java/eu/strasbourg/utils/constants/StrasbourgPortletKeys.java
@@ -120,7 +120,6 @@ public class StrasbourgPortletKeys {
 	public static final String COUNCIL_BO = "eu_strasbourg_portlet_council_CouncilBOPortlet";
 	public static final String COUNCIL_WEB = "eu_strasbourg_portlet_council_CouncilPortlet";
 
-	public static final String EJOB_BO = "eu_strasbourg_portlet_ejob_EjobBOPortlet";
 	public static final String EJOB_WEB = "eu_strasbourg_portlet_ejob_EjobPortlet";
 
     public static final String HELP_BO = "eu_strasbourg_portlet_help_HelpBOPortlet";
