1eca9552954064f81dd0b75daef8f1625ae9d136
medamineSully <ma.bourbah@sully-group.fr>
2023-06-02 16:43:09 +0200

    Migration bo : place-bo

---
+++ modules/place-bo/src/main/java/eu/strasbourg/portlet/place/constants/PlaceConstants.java
@@ -0,0 +1,37 @@
+package eu.strasbourg.portlet.place.constants;
+
+public class PlaceConstants {
+    public static final String PLACES = "places";
+    public static final String PLACES_PATH = "/place-bo-view-places.jsp";
+    public static final String EDIT_PLACE = "editPlace";
+    public static final String SAVE_PLACE = "savePlace";
+
+    public static final String PRICES = "prices";
+    public static final String PRICES_PATH = "/place-bo-view-prices.jsp";
+    public static final String EDIT_PRICE = "editPrice";
+    public static final String SAVE_PRICE= "savePrice";
+
+    public static final String PUBLIC_HOLIDAYS = "publicHolidays";
+    public static final String PUBLIC_HOLIDAYS_PATH = "/place-bo-view-public-holidays.jsp";
+    public static final String EDIT_PUBLIC_HOLIDAY = "editPublicHoliday";
+    public static final String SAVE_PUBLIC_HOLIDAY = "savePublicHoliday";
+
+    public static final String SUB_PLACES = "subPlaces";
+    public static final String SUB_PLACES_PATH = "/place-bo-view-subplaces.jsp";
+    public static final String EDIT_SUB_PLACE= "editSubPlace";
+    public static final String SAVE_SUB_PLACE = "saveSubPlace";
+
+    public static final String IMPORTS = "import";
+
+    public static final String GOOGLE = "google";
+    public static final String GOOGLE_PATH = "/place-bo-view-google.jsp";
+    public static final String EDIT_GOOGLE = "editGoogle";
+
+
+    public static final String TOKEB = "token";
+    public static final String TOKEB_PATH = "/place-bo-view-token.jsp";
+    
+    public static final String URL_PARAM_TAB = "tab";
+    public static final String URL_PARAM_MVCPATH = "mvcPath";
+    public static final String URL_PARAM_CMD = "cmd";
+}
