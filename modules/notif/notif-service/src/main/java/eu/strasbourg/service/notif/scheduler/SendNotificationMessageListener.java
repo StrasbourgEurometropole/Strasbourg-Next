a9ff7997ac3bd9e3be5c3998b307c1dcd2013790
messagR <messagr@hotmail.com>
2023-07-19 14:52:09 +0200

    Migration - Scheduler

--- modules/notif/notif-service/src/main/java/eu/strasbourg/service/notif/scheduler/SendNotificationMessageListener.java
+++ modules/notif/notif-service/src/main/java/eu/strasbourg/service/notif/scheduler/SendNotificationMessageListener.java
@@ -1,89 +1,41 @@
 package eu.strasbourg.service.notif.scheduler;
 
+import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.BaseMessageListener;
-import com.liferay.portal.kernel.messaging.DestinationNames;
-import com.liferay.portal.kernel.messaging.Message;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
-import com.liferay.portal.kernel.scheduler.SchedulerEntry;
-import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
+import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
 import com.liferay.portal.kernel.scheduler.TimeUnit;
-import com.liferay.portal.kernel.scheduler.Trigger;
-import com.liferay.portal.kernel.scheduler.TriggerFactory;
+import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
 import eu.strasbourg.service.notif.service.NotificationLocalService;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
-
-import java.util.Calendar;
-import java.util.Date;
 
 /**
  * Exporte automatiquement les campagnes au format JSON et les place dans le dossier d'import.
  */
-@Component(immediate = true, service = SendNotificationMessageListener.class)
+@Component(service = SchedulerJobConfiguration.class)
 public class SendNotificationMessageListener
-		extends BaseMessageListener {
+		implements SchedulerJobConfiguration {
 
-	@Activate
-	@Modified
-	protected void activate() {
-		String listenerClass = getClass().getName();
-
-		// Call service to be sure they are "awake"
-		this._notificationLocalService.getClass();
-
-		// Maintenant + 5 min pour ne pas lancer le scheduler au Startup du module
-		Calendar now = Calendar.getInstance();
-		now.add(Calendar.MINUTE, 5);
-		Date fiveMinutesFromNow = now.getTime();
-
+	@Override
+	public TriggerConfiguration getTriggerConfiguration() {
 		// Création du trigger "Toutes les 5 minutes"
-		Trigger trigger = _triggerFactory.createTrigger(
-				listenerClass, listenerClass, fiveMinutesFromNow, null,
-				5, TimeUnit.MINUTE);
-
-		SchedulerEntry schedulerEntry = new SchedulerEntryImpl(
-				listenerClass, trigger);
-
-		_schedulerEngineHelper.register(
-				this, schedulerEntry, DestinationNames.SCHEDULER_DISPATCH);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_schedulerEngineHelper.unregister(this);
+		return TriggerConfiguration.createTriggerConfiguration(5, TimeUnit.MINUTE);
 	}
 
 	@Override
-	protected void doReceive(Message message) throws Exception {
-		this.log.info("Start sending notifications");
-		_notificationLocalService.sendNotifications();
-		this.log.info("Finish sending notifications");
+	public UnsafeRunnable<Exception> getJobExecutorUnsafeRunnable() {
+		return () -> {
+			this.log.info("Start sending notifications");
+			_notificationLocalService.sendNotifications();
+			this.log.info("Finish sending notifications");
+		};
 	}
 
 	@Reference(unbind = "-")
 	protected void setNotificationLocalService(NotificationLocalService notificationLocalService) {
 		_notificationLocalService = notificationLocalService;
 	}
-
-	@Reference(unbind = "-")
-	protected void setSchedulerEngineHelper(
-			SchedulerEngineHelper schedulerEngineHelper) {
-
-		_schedulerEngineHelper = schedulerEngineHelper;
-	}
-
-	@Reference(unbind = "-")
-	protected void setTriggerFactory(TriggerFactory triggerFactory) {
-		_triggerFactory = triggerFactory;
-	}
-
-	private volatile SchedulerEngineHelper _schedulerEngineHelper;
 	private NotificationLocalService _notificationLocalService;
-	private TriggerFactory _triggerFactory;
 	private final Log log = LogFactoryUtil.getLog(this.getClass());
 }
