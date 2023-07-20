6ea038310adfb5dbee9e4752ce46063f20aadf4f
messagR <messagr@hotmail.com>
2023-07-20 10:18:20 +0200

    Migration - Scheduler

--- modules/gtfs/gtfs-service/src/main/java/eu/strasbourg/service/gtfs/scheduler/ImportGTFSMessageListener.java
+++ modules/gtfs/gtfs-service/src/main/java/eu/strasbourg/service/gtfs/scheduler/ImportGTFSMessageListener.java
@@ -4,34 +4,19 @@ import com.liferay.petra.function.UnsafeRunnable;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.messaging.BaseMessageListener;
-import com.liferay.portal.kernel.messaging.DestinationNames;
-import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.scheduler.SchedulerEngineHelper;
-import com.liferay.portal.kernel.scheduler.SchedulerEntry;
-import com.liferay.portal.kernel.scheduler.SchedulerEntryImpl;
 import com.liferay.portal.kernel.scheduler.SchedulerJobConfiguration;
-import com.liferay.portal.kernel.scheduler.TimeUnit;
-import com.liferay.portal.kernel.scheduler.Trigger;
 import com.liferay.portal.kernel.scheduler.TriggerConfiguration;
-import com.liferay.portal.kernel.scheduler.TriggerFactory;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import eu.strasbourg.service.gtfs.model.ImportHistoric;
 import eu.strasbourg.service.gtfs.service.ImportHistoricLocalService;
-import eu.strasbourg.utils.constants.GlobalConstants;
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 @Component(service = SchedulerJobConfiguration.class)
 public class ImportGTFSMessageListener
