08029404d41a01a6e064c99ee8a5a92fe629c562
Thomas Tse <thomas.tse@sully-group.fr>
2023-08-30 14:05:53 +0200

    Refonte strasbourg : added New home portlet

---
+++ modules/news-home-web/src/main/java/eu/strasbourg/portlet/news_home/portlet/NewsHomePortlet.java
@@ -0,0 +1,49 @@
+package eu.strasbourg.portlet.news_home.portlet;
+
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+import eu.strasbourg.portlet.news_home.constants.NewsHomePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import eu.strasbourg.portlet.news_home.context.NewsHomeDisplayContext;
+import org.osgi.service.component.annotations.Component;
+
+import java.io.IOException;
+
+/**
+ * @author thomas.tse
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=Strasbourg",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=NewsHome",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + NewsHomePortletKeys.NEWSHOME,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class NewsHomePortlet extends MVCPortlet {
+
+	@Override
+	public void render(RenderRequest request, RenderResponse response) throws IOException, PortletException {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay) request.getAttribute(WebKeys.THEME_DISPLAY);
+		NewsHomeDisplayContext dc = new NewsHomeDisplayContext(request, response);
+
+		request.setAttribute("dc", dc);
+
+		super.render(request, response);
+
+	}
+}
