b75ee0e87ab8425107b5c01f63a25ee30954f431
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2023-09-06 09:49:44 +0200

    Fix: Ajoute javax.portlet.name dans SearchAssociationPortlet pour afficher la configuration
    # Conflicts:
    #	modules/association-search-web/src/main/java/eu/strasbourg/portlet/association/SearchAssociationPortlet.java

--- modules/association-search-web/src/main/java/eu/strasbourg/portlet/association/SearchAssociationPortlet.java
+++ modules/association-search-web/src/main/java/eu/strasbourg/portlet/association/SearchAssociationPortlet.java
@@ -32,17 +32,18 @@ import java.util.Map;
 	immediate = true,
 	configurationPid = "eu.strasbourg.portlet.association.configuration.SearchAssociationConfiguration",
 	property = {
-		"javax.portlet.version=3.0",
-		"com.liferay.portlet.display-category=Strasbourg",
-		"com.liferay.portlet.instanceable=false",
-		"com.liferay.portlet.css-class-wrapper=search-association-portlet",
-		"com.liferay.portlet.single-page-application=false",
-		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/search-association-view.jsp",
-		"javax.portlet.init-param.check-auth-token=false",
-		"javax.portlet.init-param.config-template=/configuration/search-association-configuration.jsp",
-		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"}, service = Portlet.class)
+			"javax.portlet.version=3.0",
+			"com.liferay.portlet.display-category=Strasbourg",
+			"com.liferay.portlet.header-portlet-css=/css/search-association-main.css",
+			"com.liferay.portlet.instanceable=false",
+			"javax.portlet.display-name=Recherche d'association",
+			"javax.portlet.init-param.template-path=/",
+			"javax.portlet.init-param.view-template=/search-association-view.jsp",
+			"javax.portlet.init-param.config-template=/configuration/search-association-configuration.jsp",
+			"javax.portlet.init-param.check-auth-token=false",
+			"javax.portlet.name=" + StrasbourgPortletKeys.ASSOCIATION_SEARCH_WEB,
+			"javax.portlet.resource-bundle=content.Language",
+			"javax.portlet.security-role-ref=power-user,user"}, service = Portlet.class)
 public class SearchAssociationPortlet extends MVCPortlet {
 
 	@Override
