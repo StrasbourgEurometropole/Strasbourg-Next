abeee5d8075e98f6304b260c7069abc4ffb75923
messagR <messagr@hotmail.com>
2023-09-18 09:59:04 +0200

    Migration - BO (filtres par vocabulaire)

--- modules/video-bo/src/main/java/eu/strasbourg/portlet/video/display/context/ViewGalleriesDisplayContext.java
+++ modules/video-bo/src/main/java/eu/strasbourg/portlet/video/display/context/ViewGalleriesDisplayContext.java
@@ -1,41 +1,36 @@
 package eu.strasbourg.portlet.video.display.context;
 
-import com.liferay.asset.categories.item.selector.AssetCategoryTreeNodeItemSelectorReturnType;
-import com.liferay.asset.categories.item.selector.criterion.AssetCategoryTreeNodeItemSelectorCriterion;
-import com.liferay.item.selector.ItemSelector;
 import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
-import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
-import com.liferay.portal.kernel.search.*;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.*;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import eu.strasbourg.portlet.video.util.GalleryActionDropdownItemsProvider;
 import eu.strasbourg.service.video.model.VideoGallery;
 import eu.strasbourg.service.video.service.VideoGalleryLocalServiceUtil;
-import eu.strasbourg.utils.SearchHelper;
 import eu.strasbourg.utils.constants.StrasbourgPortletKeys;
+import eu.strasbourg.utils.display.context.ViewBaseDisplayContext;
 
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class ViewGalleriesDisplayContext {
+public class ViewGalleriesDisplayContext extends ViewBaseDisplayContext<VideoGallery> {
 
 	public ViewGalleriesDisplayContext(RenderRequest request,
-									   RenderResponse response, ItemSelector itemSelector) {
+									   RenderResponse response) {
+		super(request, response, VideoGallery.class);
 		_request = request;
 		_response = response;
 		_themeDisplay = (ThemeDisplay) _request.getAttribute(WebKeys.THEME_DISPLAY);
-		_httpServletRequest = PortalUtil.getHttpServletRequest(request);
-		_itemSelector = itemSelector;
 	}
 
 	/**
@@ -56,6 +51,7 @@ public class ViewGalleriesDisplayContext {
 	 *
 	 * @return SearchContainer<Event>
 	 */
+	@Override
 	public SearchContainer<VideoGallery> getSearchContainer() {
 
 		if (_searchContainer == null) {
@@ -75,8 +71,9 @@ public class ViewGalleriesDisplayContext {
 			_searchContainer.setOrderByTypeParam("orderByType");
 			_searchContainer.setOrderByCol(getOrderByCol());
 			_searchContainer.setOrderByType(getOrderByType());
+			Hits hits;
 			try {
-				getHits(this._themeDisplay.getScopeGroupId());
+				hits = getHits(this._themeDisplay.getScopeGroupId());
 
 			} catch (PortalException e) {
 				throw new RuntimeException(e);
@@ -85,8 +82,8 @@ public class ViewGalleriesDisplayContext {
 					() -> {
 						// Création de la liste d'objet
 						List<VideoGallery> results = new ArrayList<>();
-						if (_hits != null) {
-							for (Document document : _hits.getDocs()) {
+						if (hits != null) {
+							for (Document document : hits.getDocs()) {
 								VideoGallery gallery = VideoGalleryLocalServiceUtil
 										.fetchVideoGallery(GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)));
 								if (gallery != null) {
@@ -96,111 +93,13 @@ public class ViewGalleriesDisplayContext {
 						}
 
 						return results;
-					}, _hits.getLength()
+					}, hits.getLength()
 			);
 		}
 		_searchContainer.setRowChecker(
 				new EmptyOnClickRowChecker(_response));
 
 		return _searchContainer;
-	}
-	/**
-	 * Retourne les Hits de recherche pour un delta
-	 */
-	private void getHits(long groupId) throws PortalException {
-		HttpServletRequest servletRequest = PortalUtil
-				.getHttpServletRequest(_request);
-		SearchContext searchContext = SearchContextFactory
-				.getInstance(servletRequest);
-
-		// Recherche des hits
-		String keywords = ParamUtil.getString(servletRequest, "keywords");
-		_hits = SearchHelper.getBOSearchHits(searchContext,
-				getSearchContainer().getStart(),
-				getSearchContainer().getEnd(), VideoGallery.class.getName(), groupId,
-				"", keywords,
-				getOrderByColSearchField(),
-				"desc".equals(getOrderByType()));
-	}
-	/**
-	 * Renvoie le nom du champ sur laquelle on fait le tri pour
-	 * ElasticSearch
-	 *
-	 * @return String
-	 */
-	public String getOrderByColSearchField() {
-		switch (getOrderByCol()) {
-			case "title":
-			case "alias":
-				return "localized_title_fr_FR_sortable";
-			case "publication-date":
-				return "publishDate_sortable";
-			case "status":
-				return "status_sortable";
-			case "modified-date":
-			default:
-				return "modified_sortable";
-		}
-	}
-
-	/**
-	 * Renvoie la colonne sur laquelle on fait le tri
-	 *
-	 * @return String
-	 */
-	public String getOrderByCol() {
-		return ParamUtil.getString(_request, "orderByCol", "modified-date");
-	}
-
-	/**
-	 * Retourne le type de tri (desc ou asc)
-	 *
-	 * @return String
-	 */
-	public String getOrderByType() {
-		return ParamUtil.getString(_request, "orderByType", "desc");
-	}
-
-	/**
-	 * Retourne les mots clés de recherche saisis
-	 */
-	@SuppressWarnings("unused")
-	public String getKeywords() {
-		if (Validator.isNull(_keywords)) {
-			_keywords = ParamUtil.getString(_request, "keywords");
-		}
-		return _keywords;
-	}
-
-
-	public boolean hasVocabulary(String vocabularyName){
-		return getCategVocabularies().containsKey(vocabularyName);
-	}
-
-	public Map<String, String> getCategVocabularies() {
-		if (_categVocabularies == null) {
-			_categVocabularies = new HashMap<>();
-			_categVocabularies.put("vocabulary1", ParamUtil.getString(
-					_httpServletRequest, "vocabulary1", ""));
-		}
-
-		return _categVocabularies;
-	}
-
-	@SuppressWarnings("unused")
-	public String getSelectCategoriesByVocabularyIdURL(long vocabularyId) {
-		RequestBackedPortletURLFactory requestBackedPortletURLFactory =
-				RequestBackedPortletURLFactoryUtil.create(_request);
-		AssetCategoryTreeNodeItemSelectorCriterion categoryTreeNodeItemSelectorCriterion =
-				new AssetCategoryTreeNodeItemSelectorCriterion();
-		categoryTreeNodeItemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-				new AssetCategoryTreeNodeItemSelectorReturnType());
-
-		return String.valueOf(
-				_itemSelector.getItemSelectorURL(
-						requestBackedPortletURLFactory,
-						_response.getNamespace() + "selectAssetCategory",
-						categoryTreeNodeItemSelectorCriterion));
 	}
 
 	@SuppressWarnings("unused")
@@ -222,7 +121,7 @@ public class ViewGalleriesDisplayContext {
 		Hits hits = getAllHits(this._themeDisplay.getCompanyGroupId());
 
 		// Création de la liste d'objet
-		List<VideoGallery> results = new ArrayList<VideoGallery>();
+		List<VideoGallery> results = new ArrayList<>();
 		if (hits != null) {
 			for (Document document : hits.getDocs()) {
 				VideoGallery gallery = VideoGalleryLocalServiceUtil.fetchVideoGallery(GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)));
@@ -235,24 +134,6 @@ public class ViewGalleriesDisplayContext {
 	}
 
 	/**
-	 * Retourne tous les Hits de recherche
-	 */
-	private Hits getAllHits(long groupId) throws PortalException {
-		HttpServletRequest servletRequest = PortalUtil
-				.getHttpServletRequest(_request);
-		SearchContext searchContext = SearchContextFactory
-				.getInstance(servletRequest);
-
-		// Recherche des hits
-		String keywords = ParamUtil.getString(servletRequest, "keywords");
-
-		return SearchHelper.getBOSearchHits(searchContext,
-				-1, -1, VideoGallery.class.getName(), groupId,
-				"", keywords,
-				getOrderByColSearchField(),
-				"desc".equals(getOrderByType()));
-	}
-	/**
 	 * Wrapper autour du permission checker pour les permissions de module
 	 */
 	public boolean hasPermission(String actionId) throws PortalException {
@@ -261,14 +142,8 @@ public class ViewGalleriesDisplayContext {
 				StrasbourgPortletKeys.VIDEO_BO, StrasbourgPortletKeys.VIDEO_BO,
 				actionId);
 	}
-	private Hits _hits;
 	protected SearchContainer<VideoGallery> _searchContainer;
-	private Map<String, String> _categVocabularies;
-	private String _keywords;
 	private final RenderRequest _request;
 	private final RenderResponse _response;
 	protected ThemeDisplay _themeDisplay;
-	private final HttpServletRequest _httpServletRequest;
-	private final ItemSelector _itemSelector;
-	private List<VideoGallery> _galleries;
 }
--- modules/video-bo/src/main/resources/META-INF/resources/video-bo-view-videos.jsp
+++ modules/video-bo/src/main/resources/META-INF/resources/video-bo-view-videos.jsp
@@ -8,6 +8,7 @@
 	<portlet:param name="orderByType" value="${dc.orderByType}" />
 	<portlet:param name="keywords" value="${dc.keywords}" />
 	<portlet:param name="delta" value="${dc.searchContainer.delta}" />
+	<portlet:param name="filterCategoriesIdByVocabulariesName" value="${dc.filterCategoriesIdByVocabulariesName}" />
 </liferay-portlet:renderURL>
 
 <liferay-portlet:renderURL varImpl="addVideoURL">
@@ -114,6 +115,7 @@
 	<portlet:param name="orderByType" value="${dc.orderByType}" />
 	<portlet:param name="keywords" value="${dc.keywords}" />
 	<portlet:param name="delta" value="${dc.searchContainer.delta}" />
+	<portlet:param name="filterCategoriesIdByVocabulariesName" value="${dc.filterCategoriesIdByVocabulariesName}" />
 </liferay-portlet:actionURL>
 
 <liferay-portlet:actionURL name="selectionAction"
@@ -125,6 +127,7 @@
 	<portlet:param name="orderByType" value="${dc.orderByType}" />
 	<portlet:param name="keywords" value="${dc.keywords}" />
 	<portlet:param name="delta" value="${dc.searchContainer.delta}" />
+	<portlet:param name="filterCategoriesIdByVocabulariesName" value="${dc.filterCategoriesIdByVocabulariesName}" />
 </liferay-portlet:actionURL>
 
 <liferay-portlet:actionURL name="selectionAction"
@@ -136,6 +139,7 @@
 	<portlet:param name="orderByType" value="${dc.orderByType}" />
 	<portlet:param name="keywords" value="${dc.keywords}" />
 	<portlet:param name="delta" value="${dc.searchContainer.delta}" />
+	<portlet:param name="filterCategoriesIdByVocabulariesName" value="${dc.filterCategoriesIdByVocabulariesName}" />
 </liferay-portlet:actionURL>
 <liferay-portlet:renderURL varImpl="filterSelectionURL">
 	<portlet:param name="tab" value="videos" />
@@ -144,6 +148,7 @@
 	<portlet:param name="orderByType" value="${dc.orderByType}" />
 	<portlet:param name="keywords" value="${dc.keywords}" />
 	<portlet:param name="delta" value="${dc.searchContainer.delta}" />
+	<portlet:param name="filterCategoriesIdByVocabulariesName" value="${dc.filterCategoriesIdByVocabulariesName}" />
 </liferay-portlet:renderURL>
 
 <aui:script>
@@ -164,32 +169,42 @@
 		}
 	}
 
-	function getCategoriesByVocabulary(vocabularyId) {
+	function getCategoriesByVocabulary(vocabularyId, vocabularyName, categoriesId) {
+		const portletURL = "${videosURL}";
+
+		const url = Liferay.Util.PortletURL.createPortletURL(portletURL, {
+			p_p_id: "com_liferay_asset_categories_selector_web_portlet_AssetCategoriesSelectorPortlet",
+			p_p_lifecycle: 0,
+			p_p_state: "pop_up",
+			eventName: "com_liferay_asset_categories_selector_web_portlet_AssetCategoriesSelectorPortlet_selectCategory",
+			selectedCategories: categoriesId,
+			singleSelect : false,
+			vocabularyIds: vocabularyId,
+		});
+
 		Liferay.Util.openSelectionModal(
 			{
 				onSelect: function (selectedItem) {
-
-					alert("category : " + selectedItem.value.title);
 					if (selectedItem) {
-						const itemValue = selectedItem.value;
-						//submitForm(form, '${filterSelectionURL}');
-						//Liferay.SPA.app.navigate(urlString);
-
-						navigate(
-							addParams(
-								{
-									["${portletNamespace}vocabulary_" + vocabularyId]: itemValue.title,
-								},
-								PortletURLBuilder.create(getPortletURL())
-									.setParameter("vocabulary_" + vocabularyId, itemValue.title)
-									.buildString()
-									)
-						);
+						var url = "${filterSelectionURL}";
+						if(!url.includes("filterCategoriesIdByVocabulariesName"))
+							url += "&<portlet:namespace />filterCategoriesIdByVocabulariesName=";
+						if(url.includes(encodeURIComponent(vocabularyName).replaceAll("%20","+").replaceAll("'","%27")+'__')){
+							const regex = encodeURIComponent(vocabularyName).replaceAll("%20","\\+").replaceAll("'","%27") + "(.(?<!___))*___";
+							const re = new RegExp(regex, 'gi');
+							url = url.replace(re,"");
+						}
+						for(index in Object.keys(selectedItem)){
+							var selection = selectedItem[Object.keys(selectedItem)[index]];
+							url += encodeURIComponent(vocabularyName) + '__' + encodeURIComponent(selection.title) + '__' + selection.categoryId + '___';
+						}
+						submitForm(form, url);
 					}
 				},
-				selectVideoName: '<portlet:namespace />selectAssetCategory',
-				title: Liferay.Language.get('select-category'),
-				url: '${dc.getSelectCategoriesByVocabularyIdURL(vocabularyId)}'
+				selectEventName: 'com_liferay_asset_categories_selector_web_portlet_AssetCategoriesSelectorPortlet_selectCategory',
+				title: vocabularyName,
+				multiple: true,
+				url: url
 			}
 		)
 	}
