d80ed4b58eebce21827f4b9ce7c5d995dfd4d107
Jérémy Zwickert <34506261+Jzwickert@users.noreply.github.com>
2023-09-19 11:56:26 +0200

    Mise à jour du script pour prendre en compte les différents sites

--- PageComplete.groovy
+++ PageComplete.groovy
@@ -2,6 +2,7 @@ import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
 import java.util.Locale;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
@@ -10,45 +11,62 @@ import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.entry.rel.service.AssetEntryAssetCategoryRelLocalServiceUtil;
 import com.liferay.asset.entry.rel.model.AssetEntryAssetCategoryRel;
 
-	List<Long> layoutPLIDCategorie = new ArrayList<>()
-		List<AssetCategory> categories = AssetVocabularyLocalServiceUtil.fetchGroupVocabulary(GroupLocalServiceUtil.fetchGroup(PortalUtil.getDefaultCompanyId(), "strasbourg.eu").getGroupId(),"Complétion").getCategories();
-		StringBuilder sb = new StringBuilder();
-		for (AssetCategory category : categories) {
+String nomDuSite="strasbourg.eu"
 
-			List<AssetEntryAssetCategoryRel> assetEntriesRel =  AssetEntryAssetCategoryRelLocalServiceUtil.getAssetEntryAssetCategoryRelsByAssetCategoryId(category.getCategoryId());
-			sb.append("<p>Total de pages pour la catégorie "+category.getName()+" : "+assetEntriesRel.size()+"</p>");
-			sb.append("<table class=\"table table-autofit table-heading-nowrap table-list\">");
-			sb.append("<thead>");
-			sb.append("<tr class=\"portlet-section-header results-header\">");
-			sb.append("<th>Nom</th><th>Friendly URL</th>");
-			sb.append("</tr>");
-			sb.append("</thead>");
-			sb.append("<tbody>");
+Group groupSite = GroupLocalServiceUtil.fetchFriendlyURLGroup(PortalUtil.getDefaultCompanyId(), "/"+nomDuSite);
+Long siteGroupId = groupSite.getGroupId();
+List<Long> layoutPLIDCategorie = new ArrayList<>()
+long globalGroupId=GroupLocalServiceUtil.fetchFriendlyURLGroup(PortalUtil.getDefaultCompanyId(), "/global").getGroupId();
+List<AssetCategory> categories = AssetVocabularyLocalServiceUtil.fetchGroupVocabulary(globalGroupId,"Analyse de page").getCategories();
+StringBuilder sb = new StringBuilder();
+sb.append("<strong>Récapitulatif pour le site : "+groupSite.getName()+"</strong>");
+for (AssetCategory category : categories) {
 
-			for (AssetEntryAssetCategoryRel assetEntryRel: assetEntriesRel) {
-				AssetEntry assetEntry = AssetEntryLocalServiceUtil.fetchAssetEntry(assetEntryRel.getAssetEntryId());
-				Layout layout = LayoutLocalServiceUtil.fetchLayout(assetEntry.getClassPK());
-				layoutPLIDCategorie.add(layout.getPlid());
-				sb.append("<tr class=\"entry-display-style\"><td>"+layout.getName(Locale.FRANCE)+"</td><td>"+layout.getFriendlyURL(Locale.FRANCE)+"</td></tr>");
-			}
-			sb.append("</tbody>");
-			sb.append("</table>");
+	List<AssetEntryAssetCategoryRel> assetEntriesRel =  AssetEntryAssetCategoryRelLocalServiceUtil.getAssetEntryAssetCategoryRelsByAssetCategoryId(category.getCategoryId());
+	
+	StringBuilder catSB = new StringBuilder();
+	int numberOfLayoutForCat = 0;
+	for (AssetEntryAssetCategoryRel assetEntryRel: assetEntriesRel) {
+		AssetEntry assetEntry = AssetEntryLocalServiceUtil.fetchAssetEntry(assetEntryRel.getAssetEntryId());
+		Layout layout = LayoutLocalServiceUtil.fetchLayout(assetEntry.getClassPK());
+		if(layout.getGroupId() == siteGroupId) {
+			numberOfLayoutForCat++;
+			layoutPLIDCategorie.add(layout.getPlid());
+			catSB.append("<tr class=\"entry-display-style\"><td>"+layout.getName(Locale.FRANCE)+"</td><td>"+layout.getFriendlyURL(Locale.FRANCE)+"</td></tr>");
 		}
+	}
+	
+	sb.append("</br>");
+	sb.append("<p>Total de pages pour la catégorie "+category.getName()+" : "+numberOfLayoutForCat+"</p>");
+	sb.append("<table class=\"table table-autofit table-heading-nowrap table-list\">");
+	sb.append("<thead>");
+	sb.append("<tr class=\"portlet-section-header results-header\">");
+	sb.append("<th>Nom</th><th>Friendly URL</th>");
+	sb.append("</tr>");
+	sb.append("</thead>");
+	sb.append("<tbody>");
 
-		List<Layout> allLayouts = LayoutLocalServiceUtil.getLayouts(GroupLocalServiceUtil.fetchGroup(PortalUtil.getDefaultCompanyId(), "strasbourg.eu").getGroupId(),false);
-		List<Layout> layoutWithoutCat = allLayouts.stream().filter {x -> !layoutPLIDCategorie.contains(x.getPlid())}.collect();
-		sb.append("<p>Total de pages sans catégorie "+layoutWithoutCat.size()+"</p>");
-		sb.append("<table class=\"table table-autofit table-heading-nowrap table-list\">");
-		sb.append("<thead>");
-		sb.append("<tr class=\"portlet-section-header results-header\">");
-		sb.append("<th>Nom</th><th>Friendly URL</th>");
-		sb.append("</tr>");
-		sb.append("</thead>");
-		sb.append("<tbody>");
-		for (Layout layout: layoutWithoutCat) {
-				sb.append("<tr class=\"entry-display-style\"><td>"+layout.getName(Locale.FRANCE)+"</td><td>"+layout.getFriendlyURL(Locale.FRANCE)+"</td></tr>");
-		}
-		sb.append("</tbody>");
-		sb.append("</table>");
+	sb.append(catSB.toString());
+	
+	sb.append("</tbody>");
+	sb.append("</table>");
+	sb.append("</br>");
+}
+
+List<Layout> allLayouts = LayoutLocalServiceUtil.getLayouts(siteGroupId,false);
+List<Layout> layoutWithoutCat = allLayouts.stream().filter {x -> !layoutPLIDCategorie.contains(x.getPlid())}.collect();
+sb.append("<p>Total de pages sans catégorie : "+layoutWithoutCat.size()+"</p>");
+sb.append("<table class=\"table table-autofit table-heading-nowrap table-list\">");
+sb.append("<thead>");
+sb.append("<tr class=\"portlet-section-header results-header\">");
+sb.append("<th>Nom</th><th>Friendly URL</th>");
+sb.append("</tr>");
+sb.append("</thead>");
+sb.append("<tbody>");
+for (Layout layout: layoutWithoutCat) {
+		sb.append("<tr class=\"entry-display-style\"><td>"+layout.getName(Locale.FRANCE)+"</td><td>"+layout.getFriendlyURL(Locale.FRANCE)+"</td></tr>");
+}
+sb.append("</tbody>");
+sb.append("</table>");
 
 out.println(sb.toString());
