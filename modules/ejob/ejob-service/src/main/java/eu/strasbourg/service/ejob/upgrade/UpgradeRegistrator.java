d6712aa71831c63f0a818a953aefee99cb421c3b
messagR <messagr@hotmail.com>
2024-01-03 11:06:57 +0100

    Décommissionnement EJOB :    Ajout de l'upgrade step

---
+++ modules/ejob/ejob-service/src/main/java/eu/strasbourg/service/ejob/upgrade/UpgradeRegistrator.java
@@ -0,0 +1,18 @@
+package eu.strasbourg.service.ejob.upgrade;
+
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import org.osgi.service.component.annotations.Component;
+
+@Component(
+        immediate = true,
+        service = {UpgradeRegistrator.class, UpgradeStepRegistrator.class}
+)
+public class UpgradeRegistrator implements UpgradeStepRegistrator {
+
+    @Override
+    public void register(Registry registry) {
+
+        registry.register( "1.0.0", "1.1.0",
+                new eu.strasbourg.service.ejob.upgrade.v1_1_0.UpgradeSchema());
+    }
+}
