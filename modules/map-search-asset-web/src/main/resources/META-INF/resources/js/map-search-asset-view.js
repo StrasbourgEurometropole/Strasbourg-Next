fa03155546b635bdd0565a6a2f24662b7d7c48b5
messagR <messagr@hotmail.com>
2024-01-31 15:32:51 +0100

    Migration - gitlab 38942 : Tests Migration liferay 7.4 - placit carte

--- modules/map-search-asset-web/src/main/resources/META-INF/resources/js/map-search-asset-view.js
+++ modules/map-search-asset-web/src/main/resources/META-INF/resources/js/map-search-asset-view.js
@@ -20,15 +20,6 @@ var budgetMarkers = [];
 var initiativeMarkers = [];
 var eventMarkers = [];
 
-// Listes de éléments sélectionées
-var selectedProjectIds = [];
-var selectedParticipationIds = [];
-var selectedPetitionIds = [];
-var selectedSaisineIds = [];
-var selectedBudgetIds = [];
-var selectedInitiativeIds = [];
-var selectedEventIds = [];
-
 var entityType = {
 		PROJECT : 'project',
 		PARTICIPATION : 'participation',
@@ -40,12 +31,11 @@ var entityType = {
 }
 
 /**
- * Supprime l'affichage des éléments
+ * Suppression des éléments du filtre
  */
 function removeFilterElements() {
 	
 	for (var key in entityType) {
-		updateMarkerElements();
 		$("input[id^='" + entityType[key] + "_']").each(function() {
 			$(this).parent().remove();
 		});
@@ -54,10 +44,102 @@ function removeFilterElements() {
 }
 
 /**
- * Supprime l'affichage des marqueurs selon le nom de l'entité demandée
+ * (Re)création des éléments du filtre
+ */ 
+function updateFilterElements() {
+	
+	// Suppression des éléments du filtre
+    removeFilterElements();
+    
+    var checker =  "checked";
+
+	// (re)création des éléments du filtre, filtrés sur le quartier choisi ou non
+	if (projects != null) {
+		projects.forEach(function(project, index) {
+			$("fieldset[id='projects_fieldset']").append(
+				"<div>" + 
+                    "<input type='checkbox' id='project_" + project.id + "' class='hide-checkbox' value='" + project.id + "' " + checker + ">" +
+                    "<label for='project_" + project.id + "'>" + project.title + "</label>" +
+                "</div>"
+			);
+		});
+	}
+
+	if (participations != null) {
+		participations.forEach(function(participation, index) {
+			$("fieldset[id='participations_fieldset']").append(
+				"<div>" + 
+                    "<input type='checkbox' id='participation_" + participation.id + "' class='hide-checkbox' value='" + participation.id + "' " + checker + ">" +
+                    "<label for='participation_" + participation.id + "'>" + participation.title + "</label>" +
+                "</div>"
+			);
+		});
+	}
+
+	if (petitions != null) {
+		petitions.forEach(function(petition, index) {
+			$("fieldset[id='petitions_fieldset']").append(
+				"<div>" + 
+	                "<input type='checkbox' id='petition_" + petition.id + "' class='hide-checkbox' value='" + petition.id + "' " + checker + ">" +
+	                "<label for='petition_" + petition.id + "'>" + petition.title + "</label>" +
+	            "</div>"
+			);
+		});
+	}
+
+	if (saisines != null) {
+		saisines.forEach(function(saisine, index) {
+			$("fieldset[id='saisines_fieldset']").append(
+				"<div>" +
+				"<input type='checkbox' id='saisine_" + saisine.id + "' class='hide-checkbox' value='" + saisine.id + "' " + checker + ">" +
+				"<label for='saisine_" + saisine.id + "'>" + saisine.title + "</label>" +
+				"</div>"
+			);
+		});
+	}
+	
+	if (budgets != null) {
+		budgets.forEach(function(budget, index) {
+			$("fieldset[id='budgets_fieldset']").append(
+				"<div>" + 
+					"<input type='checkbox' id='budget_" + budget.id +
+						"' class='hide-checkbox' value='" + budget.id + "' " + checker + ">" +
+					"<label for='budget_" + budget.id + "'>" + budget.title + "</label>" +
+				"</div>"
+			);
+		});
+	}
+	
+	if (initiatives != null) {
+		initiatives.forEach(function(initiative, index) {
+			$("fieldset[id='initiatives_fieldset']").append(
+					"<div>" + 
+						"<input type='checkbox' id='initiative_" + initiative.id +
+						"' class='hide-checkbox' value='" + initiative.id + "' " + checker + ">" +
+						"<label for='initiative_" + initiative.id + "'>" + initiative.title + "</label>" +
+					"</div>"
+			);
+		});
+	}
+
+	if (events != null) {
+		events.forEach(function(event, index) {
+			$("fieldset[id='events_fieldset']").append(
+				"<div>" + 
+                    "<input type='checkbox' id='event_" + event.id + "' class='hide-checkbox' value='" + event.id + "' " + checker + ">" +
+                    "<label for='event_" + event.id + "'>" + event.title.fr_FR + "</label>" +
+                "</div>"
+			);
+		});
+	}
+
+}
+
+/**
+ * Suppression des marqueurs selon le nom de l'entité demandée (projets, participations...)
  */
 function removeMarkerElements(entityName) {
-    
+
 	switch (entityName) {
 		case entityType.PROJECT:
 			if (projectMarkers != null) {
@@ -67,7 +149,7 @@ function removeMarkerElements(entityName) {
 				projectMarkers = [];
 			}
 			break;
-			
+
 		case entityType.PARTICIPATION:
 			if (participationMarkers != null) {
 				participationMarkers.forEach(function(participationMarker) {
@@ -76,7 +158,7 @@ function removeMarkerElements(entityName) {
 				participationMarkers = [];
 			}
 			break;
-		
+
 		case entityType.PETITION:
 			if (petitionMarkers != null) {
 				petitionMarkers.forEach(function(petitionMarker) {
@@ -94,7 +176,7 @@ function removeMarkerElements(entityName) {
 				saisineMarkers = [];
 			}
 			break;
-			
+
 		case entityType.BUDGET:
 			if (budgetMarkers != null) {
 				budgetMarkers.forEach(function(budgetMarker) {
@@ -103,7 +185,7 @@ function removeMarkerElements(entityName) {
 				budgetMarkers = [];
 			}
 			break;
-			
+
 		case entityType.INITIATIVE:
 			if (initiativeMarkers != null) {
 				initiativeMarkers.forEach(function(initiativeMarker) {
@@ -112,7 +194,7 @@ function removeMarkerElements(entityName) {
 				initiativeMarkers = [];
 			}
 			break;
-			
+
 		case entityType.EVENT:
 			if (eventMarkers != null) {
 				eventMarkers.forEach(function(eventMarker) {
@@ -120,159 +202,17 @@ function removeMarkerElements(entityName) {
 				});
 				eventMarkers = [];
 			}
-		    break;
+			break;
 	}
 }
 
 /**
- * Mise à jour de la liste des filtres
- */ 
-function updateFilterElements(onReady) {
-	
-	// Suppression des anciens filtres
-    removeFilterElements();
-    
-    // Initialisation de la nouvelle sauvegarde des éléments séléctionnés
-    var refreshedSelectedProjectIds = [];
-    var refreshedSelectedParticipationIds = [];
-    var refreshedSelectedPetitionIds = [];
-	var refreshedSelectedSaisineIds = [];
-    var refreshedSelectedBudgetIds = [];
-    var refreshedSelectedInitiativeIds = [];
-    var refreshedSelectedEventIds = [];
-    
-    var checker;
-	
-	if (projects != null) {
-		projects.forEach(function(project, index) {
-			checker =  "";
-			if (onReady || selectedProjectIds.indexOf(project.id) > -1) {
-				$("input[id='project_" + index + "']").prop('checked', true);
-				checker = "checked";
-			}
-			$("fieldset[id='projects_fieldset']").append(
-				"<div>" + 
-                    "<input type='checkbox' id='project_" + project.id + "' class='hide-checkbox' value='" + project.id + "' " + checker + ">" +
-                    "<label for='project_" + project.id + "'>" + project.title + "</label>" +
-                "</div>"
-			);
-		});
-	}
-
-	if (participations != null) {
-		participations.forEach(function(participation, index) {
-			checker =  "";
-			if (onReady || selectedParticipationIds.indexOf(participation.id) > -1) {
-				refreshedSelectedParticipationIds.push(participation.id);
-				checker = "checked";
-			}
-			$("fieldset[id='participations_fieldset']").append(
-				"<div>" + 
-                    "<input type='checkbox' id='participation_" + participation.id + "' class='hide-checkbox' value='" + participation.id + "' " + checker + ">" +
-                    "<label for='participation_" + participation.id + "'>" + participation.title + "</label>" +
-                "</div>"
-			);
-		});
-	}
-
-	if (petitions != null) {
-		petitions.forEach(function(petition, index) {
-			checker =  "";
-			if (onReady || selectedPetitionIds.indexOf(petition.id) > -1) {
-				refreshedSelectedPetitionIds.push(petition.id);
-				checker = "checked";
-			}
-			$("fieldset[id='petitions_fieldset']").append(
-				"<div>" + 
-	                "<input type='checkbox' id='petition_" + petition.id + "' class='hide-checkbox' value='" + petition.id + "' " + checker + ">" +
-	                "<label for='petition_" + petition.id + "'>" + petition.title + "</label>" +
-	            "</div>"
-			);
-		});
-	}
-
-	if (saisines != null) {
-		saisines.forEach(function(saisine, index) {
-			checker = "";
-			if (onReady || selectedSaisineIds.indexOf(saisine.id) > -1) {
-				refreshedSelectedSaisineIds.push(saisine.id);
-				checker = "checked";
-			}
-			$("fieldset[id='saisines_fieldset']").append(
-				"<div>" +
-				"<input type='checkbox' id='saisine_" + saisine.id + "' class='hide-checkbox' value='" + saisine.id + "' " + checker + ">" +
-				"<label for='saisine_" + saisine.id + "'>" + saisine.title + "</label>" +
-				"</div>"
-			);
-		});
-	}
-	
-	if (budgets != null) {
-		budgets.forEach(function(budget, index) {
-			checker =  "";
-			if (onReady || selectedBudgetIds.indexOf(budget.id) > -1) {
-				refreshedSelectedBudgetIds.push(budget.id);
-				checker = "checked";
-			}
-			$("fieldset[id='budgets_fieldset']").append(
-				"<div>" + 
-					"<input type='checkbox' id='budget_" + budget.id +
-						"' class='hide-checkbox' value='" + budget.id + "' " + checker + ">" +
-					"<label for='budget_" + budget.id + "'>" + budget.title + "</label>" +
-				"</div>"
-			);
-		});
-	}
-	
-	if (initiatives != null) {
-		initiatives.forEach(function(initiative, index) {
-			checker =  "";
-			if (onReady || selectedInitiativeIds.indexOf(initiative.id) > -1) {
-				refreshedSelectedInitiativeIds.push(initiative.id);
-				checker = "checked";
-			}
-			$("fieldset[id='initiatives_fieldset']").append(
-					"<div>" + 
-						"<input type='checkbox' id='initiative_" + initiative.id +
-						"' class='hide-checkbox' value='" + initiative.id + "' " + checker + ">" +
-						"<label for='initiative_" + initiative.id + "'>" + initiative.title + "</label>" +
-					"</div>"
-			);
-		});
-	}
-
-	if (events != null) {
-		events.forEach(function(event, index) {
-			checker =  "";
-			if (onReady || selectedEventIds.indexOf(event.id) > -1) {
-				refreshedSelectedEventIds.push(event.id);
-				checker = "checked";
-			}
-			$("fieldset[id='events_fieldset']").append(
-				"<div>" + 
-                    "<input type='checkbox' id='event_" + event.id + "' class='hide-checkbox' value='" + event.id + "' " + checker + ">" +
-                    "<label for='event_" + event.id + "'>" + event.title.fr_FR + "</label>" +
-                "</div>"
-			);
-		});
-	}
-	
-	// Mise à jour de la sauvegarde des éléments sélectionnés
-	selectedProjectIds = refreshedSelectedProjectIds;
-	selectedParticipationIds = refreshedSelectedParticipationIds;
-	selectedPetitionIds = refreshedSelectedPetitionIds;
-	selectedBudgetIds = refreshedSelectedBudgetIds;
-	selectedInitiativeIds = refreshedSelectedInitiativeIds;
-	selectedEventIds = refreshedSelectedEventIds;
-
-}
-
-/**
- * Mise à jour de la liste des filtres
- * @param entityName Nom de l'entité à partir duquel mettre à jour
+ * Mise à jour des markers
+ * @param entityName Nom de l'entité à partir duquel mettre à jour(projets, participations...)
  */
 function updateMarkerElements(entityName) {
-    
+
+	// Suppression des markers
 	removeMarkerElements(entityName);
 	
 	switch (entityName) {
@@ -411,50 +351,13 @@ function checkPrintatorState(entityName) {
 }
 
 /**
- * Renvoi la liste des IDs des entités demandées
+ * Vérification de l'état de la checkbox d'affichage d'un élément d'une entité
  * @param 	entityName : Nom de l'entité à sonder
  * 			entityId : PK de l'élément à sonder
  * @returns Booléen
  */
 function checkMarkerState(entityName, entityId) {
 	return $("input[id^='" + entityName + "_" + entityId + "']").is(':checked');
-}
-
-/**
- * Sauvegarde les éléments sélectionnés 
- */
-function saveSelectedFilters() {
-	
-	selectedProjectIds = [];
-	selectedParticipationIds = [];
-	selectedPetitionIds = [];
-	selectedSaisineIds = [];
-	selectedBudgetIds = [];
-	selectedInitiativeIds = [];
-	selectedEventIds = [];
-	
-	$("input[id^='project_']:checked").each(function() {
-		selectedProjectIds.push(this.value);
-	});
-	$("input[id^='participation_']:checked").each(function() {
-		selectedParticipationIds.push(this.value);
-	});
-	$("input[id^='petition_']:checked").each(function() {
-		selectedPetitionIds.push(this.value);
-	});
-	$("input[id^='saisine_']:checked").each(function() {
-		selectedSaisineIds.push(this.value);
-	});
-	$("input[id^='budget_']:checked").each(function() {
-		selectedBudgetIds.push(this.value);
-	});
-	$("input[id^='initiative_']:checked").each(function() {
-		selectedInitiativeIds.push(this.value);
-	});
-	$("input[id^='event_']:checked").each(function() {
-		selectedEventIds.push(this.value);
-	});
-
 }
 
 $(document).ready(function() {
@@ -467,14 +370,14 @@ $(document).ready(function() {
     $('.leaflet-control-fullscreen-button').hide();
     
     // Requête d'affichage de l'ensemble des filtres et marqueurs
-    refreshEntitiesSelectionByDistrict(-1, true);
+    refreshEntitiesSelectionByDistrict(-1);
 });
 
 /**
- * Mettre à jour l'ensemble des éléménts (entités, filtres, marqueurs) selon le quartier choisi
+ * Mise à jour de la carto (éléments du filtre, marqueurs) selon le quartier choisi
  * @param districtId Identifiant de la catégorie du teritoire demandé en filtre
  */
-function refreshEntitiesSelectionByDistrict(districtId, onReady = false) {
+function refreshEntitiesSelectionByDistrict(districtId) {
 	AUI().use('aui-io-request', function(A) {
 		A.io.request(changeDistrictSelectionURL, {
 			method : 'post',
@@ -484,7 +387,6 @@ function refreshEntitiesSelectionByDistrict(districtId, onReady = false) {
 			},
 			on: {
                 success: function(e) {
-                	saveSelectedFilters();
                 	
                 	var data = this.get('responseData');
                 	projects = data.projects;
@@ -495,7 +397,7 @@ function refreshEntitiesSelectionByDistrict(districtId, onReady = false) {
                 	initiatives = data.initiatives;
                 	events = data.events;
                 	
-                	updateFilterElements(onReady);
+                	updateFilterElements();
                 	
                 	for (var key in entityType) {
                 		updateMarkerElements(entityType[key]);
@@ -512,11 +414,11 @@ function refreshEntitiesSelectionByDistrict(districtId, onReady = false) {
 $('#district').change(function() {
 	var selectedDistrictId = this.value;
 	
-	refreshEntitiesSelectionByDistrict(selectedDistrictId, false);
+	refreshEntitiesSelectionByDistrict(selectedDistrictId);
 });
 
 /**
- * Lors d'un acordeonage ou desacordeaonage d'une liste
+ * Lors d'un accordeonage ou desaccordeaonage d'une liste
  */
 $("input[id$='_printator_mk1']").change(function() {
 	var selectedEntity = this.value;
@@ -529,7 +431,8 @@ $("input[id$='_printator_mk1']").change(function() {
  */
 $(".pro-remove-chk").click(function() {
 	var selectedEntity = $(this).parent().siblings("input[id$='_printator_mk1']").val();
-	
+
+	// supprime les markers de l'entité
 	removeMarkerElements(selectedEntity);
 });
 
@@ -538,6 +441,8 @@ $(".pro-remove-chk").click(function() {
  */
 $(document).on('change', "fieldset[id$='_fieldset'] > div > input", function(){
 	var selectedEntity = this.id.substring(0, this.id.indexOf("_"));
-	
+	var selectedId = this.id.substring(this.id.indexOf("_")+1);
+
+	// met à jour les markers de l'entité
 	updateMarkerElements(selectedEntity);
 });
