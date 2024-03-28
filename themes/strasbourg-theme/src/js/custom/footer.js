311d978872cedd3bbfa8cfeb2843e8c4fdfb5511
Thomas Tse <thomas.tse@sully-group.fr>
2024-03-28 17:48:56 +0100

    [Refonte Strasbourg] Fixes condition to add padding to footer if #st-footer exists.

--- themes/strasbourg-theme/src/js/custom/footer.js
+++ themes/strasbourg-theme/src/js/custom/footer.js
@@ -1,3 +1,6 @@
 if(document.querySelector(".st-barre-single-sit, .st-barre-listing-sit") != undefined){
-    document.querySelector("#st-footer").classList.add("st--has-padding-bottom")
+    if(document.querySelector("#st-footer"))
+    {
+        document.querySelector("#st-footer").classList.add("st--has-padding-bottom")
+    }
 }
