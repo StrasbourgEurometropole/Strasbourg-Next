964d8bef1f5d09821cf01d32748b2251607de83e
Thomas Tse <thomas.tse@sully-group.fr>
2024-06-07 18:26:21 +0200

    [RGAA] Implemented JS to remove liferay role main and added custom id to main tag, also fixes tac crash (9.2, 12.6)

--- themes/0-global-theme/src/libs/tarteaucitron/tarteaucitron.js
+++ themes/0-global-theme/src/libs/tarteaucitron/tarteaucitron.js
@@ -197,7 +197,7 @@ var tarteaucitron = {
         },
     },
     "addFrame": function (frame_title, url, allowfullscreen, srcdoc = "", loading = "") {
-        return '<iframe title="' + frame_title + '" type="text/html" src="//www.youtube-nocookie.com/embed/' + video_id + '?' + params + '"' + (allowfullscreen == '0' ? '' : ' allowfullscreen') + ' ' + srcdoc + ' ' + loading + '></iframe>';
+        return '<iframe title="' + frame_title + '" type="text/html" src="' + url + '"' + (allowfullscreen == '0' ? '' : ' allowfullscreen') + ' ' + srcdoc + ' ' + loading + '></iframe>';
     },
     "load": function () {
         "use strict";
