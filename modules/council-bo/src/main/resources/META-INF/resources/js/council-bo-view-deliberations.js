6d575ab4784fc66730242c911b31dfbbbe26143e
Thomas Tse <thomas.tse@sully-group.fr>
2024-08-20 12:00:36 +0200

    [EVOTE] Fixed scrolling issues in evole deliberation

--- modules/council-bo/src/main/resources/META-INF/resources/js/council-bo-view-deliberations.js
+++ modules/council-bo/src/main/resources/META-INF/resources/js/council-bo-view-deliberations.js
@@ -1,5 +1,33 @@
 jQuery(function() {
     // Cache du sélecteur de delta
     $(".pagination-items-per-page").hide();
-    window.scroll(0,window.scrollY - 60);
+
+    document.addEventListener('focusin', function() {
+
+// Get the value of the specific query parameter
+        const deliberationId = getQueryParam("_eu_strasbourg_portlet_council_CouncilBOPortlet_deliberationId");
+
+// Scroll the element with the obtained ID into view
+        if (deliberationId && deliberationId !== "") {
+            scrollElementIntoViewById("delib-" + deliberationId);
+        }
+    }, { once: true });
+
+
 });
+
+
+// Function to get the value of a query parameter from the URL
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+// Function to scroll the element into view
+function scrollElementIntoViewById(id) {
+    const element = document.getElementById(id);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+}
+
