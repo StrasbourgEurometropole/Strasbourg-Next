7f205cbbe006534051cd73743acb2f9676662b29
messagR <messagr@hotmail.com>
2025-01-02 17:22:37 +0100

    gitlab 39248 : Evenement - Horodatage des horaires valeur par défaut des heures

--- modules/agenda/agenda-service/src/main/resources/eu/strasbourg/service/agenda/upgrade/v1_4_0/dependencies/update.sql
+++ modules/agenda/agenda-service/src/main/resources/eu/strasbourg/service/agenda/upgrade/v1_4_0/dependencies/update.sql
@@ -1,10 +1,10 @@
 alter table agenda_EventPeriod
-    add startTime VARCHAR(75) null AFTER endDate;
+    add startTime VARCHAR(75) default '00:00' null AFTER endDate;
 
 alter table agenda_EventPeriod
-    add endTime VARCHAR(75) null AFTER startTime;
+    add endTime VARCHAR(75) default '00:00' null AFTER startTime;
 
 alter table agenda_EventPeriod
-    add isRecurrent BOOLEAN null AFTER endTime;
+    add isRecurrent BOOLEAN default 0 null AFTER endTime;
 
 COMMIT_TRANSACTION;
